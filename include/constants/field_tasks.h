9065ca94ff549912c34af1d783c290b113f7fb47
Jaizu <jaizu85@gmail.com>
2021-08-25 13:49:12 +0200

    Remove truck sequence

--- include/constants/field_tasks.h
+++ include/constants/field_tasks.h
@@ -6,8 +6,7 @@
 #define STEP_CB_FORTREE_BRIDGE      2
 #define STEP_CB_PACIFIDLOG_BRIDGE   3
 #define STEP_CB_SOOTOPOLIS_ICE      4
-#define STEP_CB_TRUCK               5
-#define STEP_CB_SECRET_BASE         6
-#define STEP_CB_CRACKED_FLOOR       7
+#define STEP_CB_SECRET_BASE         5
+#define STEP_CB_CRACKED_FLOOR       6
 
 #endif // GUARD_CONSTANTS_FIELD_TASKS_H
--- include/field_special_scene.h
+++ include/field_special_scene.h
@@ -6,9 +6,6 @@ s16 GetTruckBoxMovement(int a1);
 void Task_Truck1(u8 taskId);
 void Task_Truck2(u8 taskId);
 void Task_Truck3(u8 taskId);
-void Task_HandleTruckSequence(u8 taskId);
-void ExecuteTruckSequence(void);
-void EndTruckSequence(u8);
 void sub_80C791C(void);
 void FieldCB_ShowPortholeView(void);
 
